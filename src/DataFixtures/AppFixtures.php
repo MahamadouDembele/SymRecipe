946486524001c17f43673dd507063ec1c0547710
MahamadouDembele <dembele.m.fst@uhp.ac.ma>
2023-04-23 02:52:53 +0000

    Premiere version du projet Symfony

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use App\Entity\Ingredient;
+use Faker\Generator;
+
+class AppFixtures extends Fixture
+{
+    private Generator $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create('fr_FR');
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        // Ingredients
+        $ingredients = [];
+        for ($i = 0; $i < 50; $i++) {
+            $ingredient = new Ingredient();
+            $ingredient->setName($this->faker->word())
+                ->setPrice(mt_rand(0, 100));
+
+            $ingredients[] = $ingredient;
+            $manager->persist($ingredient);
+        }
+
+        $manager->flush();
+    }
+}
